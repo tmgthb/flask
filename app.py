c85d65eeb81e38a60b2c098796ae0887bd80b477
Teemu Maatta <46755670+tmgthb@users.noreply.github.com>
2021-06-20 00:17:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'message'
+    render_template('info.html')
 @app.route("/info")
 def info():
     return render_template('info.html')
