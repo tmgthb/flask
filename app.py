864c883df37a105c13c48db7c59e90298d013518
Teemu Maatta <46755670+tmgthb@users.noreply.github.com>
2021-06-20 00:20:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('info.html')
-@app.route("/info")
-def info():
-    return render_template('info.html')
 
 if __name__ == '__main__':
     app.run()
