1249914645b8aabcbd8d16956835b95c853f27b9
Teemu Maatta <46755670+tmgthb@users.noreply.github.com>
2021-06-22 14:58:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ def home():
 @app.route('/info/')
 def info():
     return render_template('info.html')
+@app.route('/dashboard/')
+def dashboard():
+    return render_template('dashboard.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port) #https://github.com/dpgaspar/Flask-AppBuilder/issues/733#issuecomment-379009480
